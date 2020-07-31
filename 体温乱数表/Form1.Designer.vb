266f1ab669113b0fae5c0fc1c8671234f239bd3c
yosshii50 <kasaubunts@gmail.com>
2020-07-31 13:53:33 +0900

    Initial commit

---
+++ 体温乱数表/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "生成"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(12, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(35, 19)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "36.0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(76, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(35, 19)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "37.0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(53, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "～"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(156, 12)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(35, 19)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = "0.1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(117, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "STEP"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(411, 384)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "体温乱数表"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ 体温乱数表/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+
+    Dim LabelCtlList As List(Of Label)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        LabelCtlList = CreateLabelControl(Me, Label1)
+
+    End Sub
+
+    Private Function CreateLabelControl(ByVal BaseForm As Form, ByVal BaseControl As Label) As List(Of Label)
+
+        Dim WrkList As New List(Of Label)
+        WrkList.Add(BaseControl)
+
+        For WrkY As Integer = 0 To 10
+            For WrkX As Integer = 0 To 10
+
+                Dim WrkControl As Label
+
+                '左上端は基準コントロールなので何もしない
+                If WrkY = 0 And WrkX = 0 Then
+                    Continue For
+                End If
+
+                'コントロール生成
+                WrkControl = New Label
+                WrkList.Add(WrkControl)
+
+                If WrkY = 0 Then
+                    '列タイトルの番号
+                    WrkControl.Text = WrkX
+                ElseIf WrkX = 0 Then
+                    '行タイトルの番号
+                    WrkControl.Text = WrkY
+                End If
+
+                WrkControl.AutoSize = WrkList(0).AutoSize
+                WrkControl.BorderStyle = WrkList(0).BorderStyle
+                WrkControl.TextAlign = WrkList(0).TextAlign
+                WrkControl.Size = WrkList(0).Size
+
+                WrkControl.Location = New Point(WrkList(0).Left + WrkX * WrkList(0).Width, WrkList(0).Top + WrkY * WrkList(0).Height)
+
+                'フォームに追加
+                BaseForm.Controls.Add(WrkControl)
+            Next
+        Next
+
+        Return WrkList
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Call ResetLabelTest(LabelCtlList, CDbl(TextBox1.Text), CDbl(TextBox2.Text), CDbl(TextBox3.Text))
+
+    End Sub
+
+    Private Sub ResetLabelTest(ByVal WrkList As List(Of Label), ByVal StartNo As Double, ByVal EndNo As Double, ByVal StepNo As Double)
+
+        Static rnd As New System.Random(Now.Second * 100 + Now.Minute + Now.Hour)
+
+        For Each WrkLabel As Label In WrkList
+
+            If WrkList(0).Top = WrkLabel.Top Then
+                Continue For
+            End If
+            If WrkList(0).Left = WrkLabel.Left Then
+                Continue For
+            End If
+
+            Dim TmpDbl As Double
+
+            TmpDbl = EndNo - StartNo
+            TmpDbl = TmpDbl / StepNo
+            TmpDbl = Math.Truncate(TmpDbl)
+            TmpDbl = rnd.Next(TmpDbl)
+            TmpDbl = TmpDbl * StepNo
+            TmpDbl = StartNo + TmpDbl
+
+            WrkLabel.Text = TmpDbl
+
+        Next
+
+    End Sub
+
+End Class
